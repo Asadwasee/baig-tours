5cbd7d321324a0c95918fe54b278c423f4ee4666
AfreenAsmat <afreenasmat7@gmail.com>
2026-07-14 19:22:57 +0500

    Initialize frontend project and glbal layout

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Baig Tours",
+  description: "Tour and Travel Booking Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} ${poppins.variable}`}
+    >
+      <body className="flex min-h-screen flex-col">
+  <Navbar />
+
+  <main className="flex-1">
+    {children}
+  </main>
+
+  <Footer />
+</body>
+    </html>
+  );
+}
---
+++ frontend/src/components/common/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 h-20 flex items-center">
+        Navbar
+      </div>
+    </nav>
+  );
+}
