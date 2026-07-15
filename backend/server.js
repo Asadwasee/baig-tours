0b4cff8546bc91870d71693909b46274f662b26c
wasif7619 <lifeboy0077@gmail.com>
2026-07-15 11:53:15 +0500

    Blog Crud Apis Done

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,38 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import connectDB from './config/db.js';
-import dns from 'dns';
+import blogRoutes from './routes/blogRoutes.js';
 
-dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-// Environment variables load karein
+// Environment variables
 dotenv.config();
 
 // Database connection
 connectDB();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve uploaded images
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/blogs', blogRoutes);
 
 // Base Test Route
 app.get('/', (req, res) => {
   res.send('Baig Tours MERN Backend is running smoothly...');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
